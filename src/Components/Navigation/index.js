e954ae9bceca038ed5e92f62cf4b1168eb965e3c
Carlos Rodrigo <rodrigolimac137@gmail.com>
2022-01-04 19:34:10 -0300

    Starting Navigation Box

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -1,9 +1,17 @@
+import BoxNavigation from "./style";
+import Nav from "./style";
+
 function Navigation (){
     return (
-        <div >
-            <img />
-    <div>Home Destination Crew Tecnology</div>
-        </div>
+        <BoxNavigation >
+            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48"><g fill="none" fill-rule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF"/><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/></g></svg>
+            <Nav>
+                <button>Home</button>
+                <button>Destination</button>
+                <button>Crew</button>
+                <button>Tecnology</button>
+            </Nav>
+        </BoxNavigation>
     );
 }
 
---
+++ src/Components/Navigation/style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const BoxNavigation = styled.div `
+    height: 96px;
+    margin-top: 40px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Nav = styled.div `
+    width: 830px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 48px;
+`;
+
+export default BoxNavigation && Nav;
